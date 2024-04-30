5ea66e403b84c1cf64ba9d65bd6ec12228cae642
Lucas Fernandes Marinho <1525533@sga.pucminas.br>
2024-04-30 08:00:28 -0300

    Novos códigos

---
+++ PROGRAMAS EM C/EX BEECROWD/Fibonacci em Vetor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int T;
+    int *FIB;
+    int i=2,n1=0,n2=1;
+
+    scanf("%d",&T);
+
+    FIB = (int*) malloc(T * sizeof(int));
+
+    FIB[0] = 0;
+    FIB[1] = 1;
+
+    for( ;i<=T;i++){
+        FIB[i] = n1+n2;
+        n1 = n2;
+        n2 += n1;
+    }
+
+    printf("Fib[%d] = %d",T,FIB[T]);
+
+    free(FIB);
+
+    return 0;
+}
