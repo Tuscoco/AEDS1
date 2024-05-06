0a1c4d49cbddd49c91a411f41f471a3675d7aba5
Lucas Fernandes Marinho <1525533@sga.pucminas.br>
2024-05-06 09:45:43 -0300

    Modificações

--- PROGRAMAS EM C/EX LIVRO/CAP6/EX3.c
+++ PROGRAMAS EM C/EX LIVRO/CAP6/EX3.c
@@ -1,34 +1,44 @@
 //INCLUSÃO DE BIBLIOTECA
 #include <stdio.h>
+#include <stdlib.h>
+
+//FUNÇÃO PARA INTERCALAR
+int *intercalar(const int *v1,const int *v2){
+    int *vet = (int*) malloc(20*sizeof(int));
+    int j=0;
+
+    for(j=0;j<20;j++){
+        vet[2*j] = v1[j];
+        vet[2*j+1] = v2[j];
+    }
+    
+    return vet;
+}
 
 //FUNÇÃO PRINCIPAL
 int main()
 {
     //DECLARAÇÃO DE VARIÁVEIS
-    int vet1[10],vet2[10],vet3[20];
-    int i=0,j=0;
+    int vet1[10],vet2[10],*vet3;
+    int i=0;
 
-    //ENTRADA DE DADOS NOS VETORES
+    //ENTRADA DE DADOS NO VETOR 1
     for(i=0;i<10;i++){
         printf("Informe um numero:\n");
         scanf("%d",&vet1[i]);
+    }
+
+    //ENTRADA DE DADOS NO VETOR 2
+    for(i=0;i<10;i++){
         printf("Informe um numero:\n");
         scanf("%d",&vet2[i]);
     }
 
-    //INTERCALAR
-    for(i=0;i<10;i++){
-        vet3[j]=vet1[i];
-        vet3[j+1]=vet2[i];
-        j++;
+    //SAÍDA DE DADOS
+    vet3 = intercalar(vet1,vet2);
+    for(i=0;i<20;i++){
+        printf("%d--",vet3[i]);
     }
 
-    //SAÍDA DE DADOS
-    for(i=0;i<20;i++){
-        printf("Vet3[%d] = %d\n",i,vet3[i]);
-    }
-    
     return 0;
 }
-
-////////////////ERRADO DESGRAÇAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
