7065020a9f182643d7b417c4d06e71b2e97047d6
Lucas Fernandes Marinho <1525533@sga.pucminas.br>
2024-05-08 08:20:40 -0300

    Tudo ta dando errado

--- PROGRAMAS EM C/EX CANVAS/LISTA 6/EX2.c
+++ PROGRAMAS EM C/EX CANVAS/LISTA 6/EX2.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void preencher_Vetor(){
-    int vetor[10];
-    int i=0;
-
-    for(i=0;i<10;i++){
-        printf("Informe um numero:\n");
-        scanf("%d",&vetor[i]);
-    }
-    negativos(vetor);
-}
+int contn=0;
 
 int *negativos(int *veto){
-    int contn=0,i=0;
+    int i=0;
 
     for(i=0;i<10;i++){
         if(veto[i]<0){
@@ -29,10 +20,32 @@ int *negativos(int *veto){
         }
     }
 
-    return *veto;
+    return v;
+}
+
+void preencher_Vetor(){
+    int vetor[10];
+    int i=0;
+    int *v;
+
+    for(i=0;i<10;i++){
+        printf("Informe um numero:\n");
+        scanf("%d",&vetor[i]);
+    }
+
+    *v=*negativos(vetor);
+
+    for(i=0;i<contn;i++){
+        printf("%d\n",v[i]);
+    }
+
+    free(v);
 }
 
 int main(){
+    preencher_Vetor();
+
+    return 0;
 }
 
 /////////////DUVIDA
--- PROGRAMAS EM C/EX CANVAS/LISTA 6/EX3.c
+++ PROGRAMAS EM C/EX CANVAS/LISTA 6/EX3.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
+void intercalar(int *v1,int *v2){
+    int i=0,j=0,z=0;
+    int vetor3[10];
+
+    for(j=0,i=0;j<10;j+=2,i++){
+        vetor3[j] = v2[i]; 
+    }
+
+    for(j=1,i=0;j<10;j+=2,i++){
+        vetor3[j] = v1[i];
+    }
+
+    for(i=0;i<10;i++){
+        printf("Vetor3[%d] = %d\n",i,vetor3[i]);
+    }
+}
+
 void preencher_Vetores(){
     int vetor1[5];
     int vetor2[5];
@@ -14,4 +31,14 @@ void preencher_Vetores(){
         printf("Informe um numero:\n");
         scanf("%d",&vetor2[i]);
     }
+
+    intercalar(*vetor1,*vetor2);
 }
+
+int main(){
+    preencher_Vetores();
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////zsenul7iyucui
