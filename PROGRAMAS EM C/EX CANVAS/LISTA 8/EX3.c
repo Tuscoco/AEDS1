13b4e0f8cd23782c8415af56a7bed52e150c1ad0
Lucas Fernandes Marinho <lucasfernandesmarinho7@gmail.com>
2024-05-13 23:47:52 -0300

    Novos Códigos

--- PROGRAMAS EM C/EX CANVAS/LISTA 8/EX3.c
+++ PROGRAMAS EM C/EX CANVAS/LISTA 8/EX3.c
@@ -6,21 +6,36 @@ struct registro{
     char nome[50];
     int horas;
     char pagina;
-    double total = 0;
+    double total;
 };
 typedef struct registro registro;
 
 
 void relatorio(registro *vet){
     int j=0;
+    int cont=0;
 
     for(j=0;j<5;j++){
+        vet[j].total = 0;
         if(vet[j].horas < 20){
             vet[j].total += 35;
             if(vet[j].pagina == 'S'){
                 vet[j].total += 40;
             }
-        }else{}
+        }else{
+            cont = vet[j].horas - 20;
+            cont*=2.5;
+            vet[j].total += 35 + cont;
+            if(vet[j].pagina == 'S'){
+                vet[j].total += 40;
+            } 
+        }
+    }
+
+    for(j=0;j<5;j++){
+        printf("----------%s----------\n",vet[j].nome);
+        printf("Valor a pagar: %.2lf\n",vet[j].total);
+        printf("------------------------------------\n");
     }
 }
 
@@ -34,11 +49,23 @@ void registrar(){
         scanf("%s", cliente[i].nome);
         printf("Informe a quantidade de horas de acesso:\n");
         scanf("%d",&cliente[i].horas);
+
+        while (getchar() != '\n');
+
+
         printf("O cliente possui pagina(S ou N):\n");
         scanf("%c",cliente[i].pagina);
+
+        while (getchar() != '\n');
     }
 
     relatorio(cliente);
 }
 
-////////INCOMPLETO
+
+int main()
+{
+    registrar();
+
+    return 0;
+}
