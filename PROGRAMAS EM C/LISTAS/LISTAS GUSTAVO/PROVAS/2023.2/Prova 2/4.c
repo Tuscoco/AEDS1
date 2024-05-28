6e77d5b8911933bcf3d459e30713b86b2fc9e8e8
Lucas Fernandes Marinho <1525533@sga.pucminas.br>
2024-05-28 08:18:20 -0300

    Novos Códigos

--- PROGRAMAS EM C/LISTAS/LISTAS GUSTAVO/PROVAS/2023.2/Prova 2/4.c
+++ PROGRAMAS EM C/LISTAS/LISTAS GUSTAVO/PROVAS/2023.2/Prova 2/4.c
@@ -9,30 +9,48 @@ typedef struct{
 
 typedef struct{
     pessoa *cliente;
-    int b,a,c;
+    int b,a,c;;
 }conta;
 
-pessoa cadastrar(const char *nome, const int d, const int m, const int a)
+pessoa dadosPessoais(const char *nome, const int d, const int m, const int a)
 {
-    pessoa cliente;
+    pessoa novo;
 
-    strcpy(cliente.nome,nome);
+    strcpy(novo.nome,nome);
 
-    cliente.d = d;
-    cliente.m = m;
-    cliente.a = a;
+    novo.d = d;
+    novo.m = m;
+    novo.a = a;
 
-    return cliente;
+    return novo;
 }
 
-conta Cconta(const pessoa *cl, const int b, const int a, const int c)
+conta criarConta(pessoa *cliente, int b, int a, int c)
 {
-    conta nova;
-    
-    nova.cliente = cl;
-    nova.b = b;
-    nova.a = a;
-    nova.c = c;
+    conta novo;
 
-    return nova;
+    novo.cliente = cliente;
+    novo.b = b;
+    novo.a = a;
+    novo.c = c;
+
+    return novo;
+}
+
+int main()
+{
+    pessoa c1;
+    conta b1;
+    char nome[100] = "Joaquim Silva";
+    int dia=1,mes=5,ano=2000,banco=1,agencia=8731,conta=12489;
+
+    c1 = dadosPessoais(nome,dia,mes,ano);
+    b1 = criarConta(&c1,banco,agencia,conta);
+
+    system("cls");
+
+    printf("Cliente: %s\n",b1.cliente->nome);
+    printf("Banco: %d\n",b1.b);
+
+    return 0;
 }
