78e67cbf4a9643a834d7e5c657d3b3568e115ffa
Lucas Fernandes Marinho <lucas.fernandes@sga.pucminas.br>
2024-06-06 22:11:48 -0300

    Novos testes

---
+++ PROGRAMAS EM C++/TESTES/entrada.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+
+    int x;
+    int y;
+
+    cout << "Insira um numero:\n";
+    cin >> x;
+
+    cout << "Insira outro:\n";
+    cin >> y;
+
+    system("cls");
+
+    cout << x+y;
+
+    return 0;
+}
